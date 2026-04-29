f3b7c66397724e92aee149fca30aed758469dedb
LuisT <ldterrones.25@est.ucab.edu.ve>
2026-04-29 17:09:41 -0400

    VERSION 3 - (SIMPLIFICADA) V7

---
+++ src/main/java/com/shopsmart/model/CartItem.java
@@ -0,0 +1,16 @@
+package com.shopsmart.model;
+
+public class CartItem {
+    private Producto producto;
+    private int cantidad;
+
+    public CartItem(Producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() { return producto; }
+    public int getCantidad() { return cantidad; }
+    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
+    public double getSubtotal() { return producto.getPrecio() * cantidad; }
+}
